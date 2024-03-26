0f735228e454f74a6a8a56250a9d5634b099e38d
Jang Sungwo <sungwoo515@gmail.com>
2024-03-26 17:56:09 +0900

    1,2

---
+++ src/main/java/hello/hellospring/HelloSpringApplication.java
@@ -0,0 +1,14 @@
+package hello.hellospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloSpringApplication.class, args);
+	}
+	//톰켓이라는 웹 서버를 띄움
+
+}
