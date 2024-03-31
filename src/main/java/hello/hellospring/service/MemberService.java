52d5093637650ec916d9a6eac325e91b0c10eef1
sungw0o <sungwoo515@gmail.com>
2024-03-31 21:28:09 +0900

    Chapter 3

---
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -0,0 +1,34 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+
+import java.util.*;
+
+public class MemberService {
+    private final MemberRepository memberRepository;
+    // 회원 가입
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+    public Long join(Member member) {
+        extracted(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void extracted(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+    //전체 회원 가입 조히ㅗ
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+    public Optional<Member> findOne(Long memberId){
+        return memberRepository.findById(memberId);
+    }
+}
